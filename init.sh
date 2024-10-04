0e0b8f71f82d54083aeb77a142c296c0bcb88902
gnshb <ch22b002@smail.iitm.ac.in>
2024-10-04 17:55:35 +0530

    Update on 10/04/24 at 17:55:35 by

--- init.sh
+++ init.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "Script to setup your LDAP credentials";
-    echo "Usage: sh init.sh username password";
-    exit 1
-elif [ ! $# -eq 2 ]
-  then
-    echo "Invalid format!";
-    exit 1
+if [ $# -eq 0 ]; then
+	echo "Script to setup your LDAP credentials";
+    	echo "Usage: sh init.sh username password";
+    	exit 1
+elif [ ! $# -eq 2 ]; then
+	echo "Invalid format!";
+    	exit 1
 fi
 
 sed -i -e "s/.*const username.*/const username = \"$1\";/g" -e "s/.*const password.*/const password = \"$(echo $2 | sed 's/\\/\\\\/g')\";/g" ~/.assn-cli/main.js;
